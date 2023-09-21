3c97154ceb299275c11c801154633b3cbc7ac701
Nahid1911 <nahid.raihan@gmail.com>
2023-09-22 02:02:50 +0600

    Add join table migration file

---
+++ db/migrate/20230919155904_create_join_table_tran_details_tran_groups.rb
@@ -0,0 +1,11 @@
+class CreateJoinTableTranDetailsTranGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tran_details_tran_groups, id: false do |t|
+      t.bigint :tran_detail_id
+      t.bigint :tran_group_id
+    end
+
+    add_index :tran_details_tran_groups, :tran_detail_id
+    add_index :tran_details_tran_groups, :tran_group_id
+  end
+end
